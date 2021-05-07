3fbf4d386f209c08dca20f621f7b03ef732c5aea
anasoid <oufdou.anas@gmail.com>
2021-05-07 13:07:10 +0000

    add raf 2/3 listner

---
+++ jmeter-as-code-core/src/test/java/org/anasoid/jmeter/as/code/core/wrapper/jmeter/visualizers/SimpleDataWriterWrapperXmlTest.java
@@ -0,0 +1,49 @@
+package org.anasoid.jmeter.as.code.core.wrapper.jmeter.visualizers;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import org.anasoid.jmeter.as.code.core.AbstractJmcTest;
+import org.anasoid.jmeter.as.code.core.test.utils.SetterTestUtils;
+import org.anasoid.jmeter.as.code.core.wrapper.jmeter.testelement.TestPlanWrapper;
+import org.junit.jupiter.api.Test;
+/*
+ * Copyright 2020-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * @author : anas
+ * Date :   07-May-2021
+ */
+
+class SimpleDataWriterWrapperXmlTest extends AbstractJmcTest {
+  private static final String PARENT_PATH =
+      "org/anasoid/jmeter/as/code/core/wrapper/jmeter/visualizers";
+
+  private static final String NODE_NAME = "ResultCollector";
+
+  @Test
+  void testSetter()
+      throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+    SetterTestUtils.testSetter(SimpleDataWriterWrapper.builder());
+  }
+
+  @Test
+  void testDefault() throws IOException {
+    TestPlanWrapper testPlanWrapper =
+        TestPlanWrapper.builder()
+            .withName("Test Plan")
+            .addListener(SimpleDataWriterWrapper.builder().withName("Simple Data Writer").build())
+            .build();
+
+    checkWrapper(testPlanWrapper, PARENT_PATH + "/simpleDataWriter.default.jmx", NODE_NAME);
+  }
+}
